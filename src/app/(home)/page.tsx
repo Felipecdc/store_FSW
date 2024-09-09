743c4f142b0cd821ec8d6c9e11a81ad955f4a218
Felipe Castro <felipecdc09@gmail.com>
2024-09-09 18:20:58 -0300

    feat: add third promo banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -25,17 +25,17 @@ export default async function Home() {
   const categories = await prismaClient.category.findMany({});
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src={"/banner-home-01.png"}
         alt="Até 55% de desconto esse mes."
       />
 
-      <div className="mt-8 px-5">
+      <div className="px-5">
         <Categories categories={categories} />
       </div>
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
@@ -45,10 +45,15 @@ export default async function Home() {
         alt="Até 55% de desconto em mouses."
       />
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
       </div>
+
+      <PromoBanner
+        src={"/banner-home-03.png"}
+        alt="Até 55% de desconto em fones."
+      />
     </div>
   );
 }
