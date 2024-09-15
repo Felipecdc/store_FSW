638f1f4be885145564eb48a8b51b495dbaac863f
Felipe Castro <felipecdc09@gmail.com>
2024-09-15 18:52:04 -0300

    feat: ensure products are fetched on product page

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,4 +1,8 @@
-const ProductsPage = () => {
+import { prismaClient } from "@/lib/prisma";
+
+const ProductsPage = async () => {
+  const products = await prismaClient.product.findMany({});
+
   return (
     <div>
       <h1>ds</h1>
