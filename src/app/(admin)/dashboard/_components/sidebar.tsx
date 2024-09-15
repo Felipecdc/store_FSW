de06123c48d8bf9a6f1b4f6976b23d7f16c67d69
Felipe Castro <felipecdc09@gmail.com>
2024-09-15 18:50:48 -0300

    feat: ensure products buttons have correct background on select

--- src/app/(admin)/dashboard/_components/sidebar.tsx
+++ src/app/(admin)/dashboard/_components/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   LayoutDashboardIcon,
@@ -5,28 +7,53 @@ import {
   PackageIcon,
   PackageSearchIcon,
 } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const path = usePathname();
+  console.log(path);
+
   return (
     <div className="flex min-w-[300px] flex-col items-center gap-8 border-r border-solid border-accent bg-background p-8">
       <h1 className="text-lg font-semibold">
         <span className="text-primary">Name</span> Store
       </h1>
       <div className="flex w-full flex-col gap-3">
-        <Button variant={"outline"} className="flex w-full justify-start gap-2">
-          <LayoutDashboardIcon size={16} />
-          Dashboard
-        </Button>
-        <Button variant={"outline"} className="flex w-full justify-start gap-2">
-          <PackageIcon size={16} />
-          Produtos
-        </Button>
-        <Button variant={"outline"} className="flex w-full justify-start gap-2">
-          <ListOrderedIcon size={16} /> Categorias
-        </Button>
-        <Button variant={"outline"} className="flex w-full justify-start gap-2">
-          <PackageSearchIcon size={16} /> Pedidos
-        </Button>
+        <Link href={"/dashboard"}>
+          <Button
+            variant={"outline"}
+            className={`flex w-full justify-start gap-2 ${path.includes("/dashboard") && "bg-primary text-white hover:bg-primary"} `}
+          >
+            <LayoutDashboardIcon size={16} />
+            Dashboard
+          </Button>
+        </Link>
+        <Link href={"/dashboard/products"}>
+          <Button
+            variant={"outline"}
+            className={`flex w-full justify-start gap-2 ${path.includes("/products") && "bg-primary text-white hover:bg-primary"} `}
+          >
+            <PackageIcon size={16} />
+            Produtos
+          </Button>
+        </Link>
+        <Link href={"/dashboard/catalog"}>
+          <Button
+            variant={"outline"}
+            className={`flex w-full justify-start gap-2 ${path.includes("/catalog") && "bg-primary text-white hover:bg-primary"} `}
+          >
+            <ListOrderedIcon size={16} /> Categorias
+          </Button>
+        </Link>
+        <Link href={"/dashboard/orders"}>
+          <Button
+            variant={"outline"}
+            className={`flex w-full justify-start gap-2 ${path.includes("/orders") && "bg-primary text-white hover:bg-primary"} `}
+          >
+            <PackageSearchIcon size={16} /> Pedidos
+          </Button>
+        </Link>
       </div>
     </div>
   );
